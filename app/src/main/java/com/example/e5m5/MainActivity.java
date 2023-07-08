ba7cd0fd3e74b140c5911426c1f84ba734600dfe
Margaret <maenunezt@gmail.com>
2023-07-08 14:04:06 -0400

    ejercicio 5

--- app/src/main/java/com/example/e5m5/MainActivity.java
+++ app/src/main/java/com/example/e5m5/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.e5m5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.e5m5.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Aquí hay ratas de dos patas", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
