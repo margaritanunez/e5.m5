4a5694d45e7e3240152728ba252de362e3f48d2e
Margaret <maenunezt@gmail.com>
2023-07-08 12:27:09 -0400

    ejercicio 5

---
+++ app/src/main/java/com/example/e5m5/ListFragment.java
@@ -0,0 +1,88 @@
+package com.example.e5m5;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.e5m5.databinding.FragmentListBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListFragment extends Fragment {
+    private FragmentListBinding binding;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ListFragment newInstance(String param1, String param2) {
+        ListFragment fragment = new ListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentListBinding.inflate(getLayoutInflater(), container, false);
+        WordAdapter wordAdapter = new WordAdapter();
+        wordAdapter.setRatNames(getData());
+        binding.listRecyclerView.setAdapter(wordAdapter);
+        binding.listRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getBaseContext()));
+
+        return binding.getRoot();
+    }
+    private List<String> getData(){
+        ArrayList<String> ratNames = new ArrayList<>();
+        ratNames.add("Andrés");
+        ratNames.add("Patricio");
+        ratNames.add("Javiera");
+        ratNames.add("Homero");
+        ratNames.add("Floripondio");
+        return ratNames;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/e5m5/WordAdapter.java
@@ -0,0 +1,49 @@
+package com.example.e5m5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.e5m5.databinding.ItemBinding;
+
+import java.util.List;
+
+public class WordAdapter extends RecyclerView.Adapter<WordAdapter.ViewHolder>{
+    private List<String> ratNames;
+    public void setRatNames(List<String> ratNames){
+        this.ratNames= ratNames;
+    }
+    @NonNull
+    @Override
+    public WordAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemBinding binding = ItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordAdapter.ViewHolder holder, int position) {
+        String rat = ratNames.get(position);
+        holder.binding(rat);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ratNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemBinding binding;
+        public ViewHolder(ItemBinding binding) {
+            super(binding.getRoot());
+        }
+
+        public void binding(String rat) {
+            binding.ratasTxt.setText(rat);
+        }
+    }
+}
